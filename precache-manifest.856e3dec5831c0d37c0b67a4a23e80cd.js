448f0d21911d60b31589538d90a02236d9d3e573
cabejoss <arifkur10027@gmail.com>
2020-04-21 19:53:18 +0700

    Updates

---
+++ precache-manifest.856e3dec5831c0d37c0b67a4a23e80cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ea618c6e55dfb23e4a75452e8fe8ae",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "4611ee705283f2932eb2",
+    "url": "/covid/static/css/main.d67cabfd.chunk.css"
+  },
+  {
+    "revision": "ac6b081ef805402d1518",
+    "url": "/covid/static/js/2.97d2817a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.97d2817a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4611ee705283f2932eb2",
+    "url": "/covid/static/js/main.b6a61af6.chunk.js"
+  },
+  {
+    "revision": "7f895a77882eda8f1ef7",
+    "url": "/covid/static/js/runtime-main.49d2fe06.js"
+  }
+]);
